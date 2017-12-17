000812f3cb7eaea2f10bc9ed7dbae751fd8782ec
Super Frank <fmjladage@gmail.com>
2017-12-17 16:54:48 +0100

    second commit

---
+++ Tiled Sprite Map/YumaTerrainTrans.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="YumaTerrain" tilewidth="24" tileheight="24" tilecount="100" columns="10">
+ <image source="24x24TerrainSpriteMap.png" width="240" height="240"/>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="4" x="0" y="-0.0222222" width="23.9778" height="24.0222"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="23.9778" height="23.9778"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="4" x="-0.0222222" y="-0.0222222" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+</tileset>
