75ac5f06fef4827a21ed10bc5dc9b53f4a35508e
Super Frank <fmjladage@gmail.com>
2017-12-18 15:15:54 +0100

    3

---
+++ Assets/Scripts/MovementObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovementObject : MonoBehaviour {
+
+    Animator anim;
+
+    public Vector2 targetPosition;
+
+    //Movement Speed
+    //public float moveTime = 0.1f;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Mouseclick to move to location
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        //Walk Animation
+        if (targetPosition != Vector2.zero)
+        {
+            anim.SetBool("iswalking", true);
+            anim.SetFloat("input_x", targetPosition.x);
+            anim.SetFloat("input_y", targetPosition.y);
+        }
+        else
+        {
+            anim.SetBool("iswalking", false);
+        }
+
+        //Movement Speed Formula
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * 2);
+    }
+}
